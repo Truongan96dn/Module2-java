84fa44bc06595aec6d5b49722f40dd7c076644ba
Truongan96dn <hotruongandn@gmail.com>
2023-03-10 16:14:56 +0700

    format code

--- module_2/src/access_modifier/bai_tap/student/Student.java
+++ module_2/src/access_modifier/bai_tap/student/Student.java
@@ -1,21 +1,25 @@
 package access_modifier.bai_tap.student;
 
 public class Student {
-    private String name = "John";
-    private String classes = "C02";
-    public Student(){}
-    protected String getName(){
+    private String name = "Athena";
+    private String classes = "C01";
+
+    public Student() {
+    }
+
+    protected String getName() {
         return this.name;
     }
-    protected String getClasses(){
+
+    protected String getClasses() {
         return this.classes;
     }
 
-    protected void setName(String name ) {
+    protected void setName(String name) {
         this.name = name;
     }
 
     protected void setClasses(String classes) {
-         this.classes = classes;
+        this.classes = classes;
     }
 }
