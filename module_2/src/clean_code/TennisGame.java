26c0eee31ddf9da864b583250138c72dffc21a3f
Truongan96dn <hotruongandn@gmail.com>
2023-03-15 13:55:34 +0700

    format code

--- module_2/src/clean_code/TennisGame.java
+++ module_2/src/clean_code/TennisGame.java
@@ -4,18 +4,20 @@ public class TennisGame {
 
     public static String getScore(String player1, String player2, int score1, int score2) {
         String score = "";
-        if (score1==score2)
-        {isSamePoint(score1);}
-        else if (score1>=4 || score2>=4)
-        {over3Point(score1,score2);}
-        else
-        {under3Point(score1,score2);}
+        if (score1 == score2) {
+            isSamePoint(score1);
+        } else if (score1 >= 4 || score2 >= 4) {
+            over3Point(score1, score2);
+        } else {
+            under3Point(score1, score2);
+        }
         return score;
     }
-    public static String isSamePoint( int score1){
+
+    //khi 2 người chơi bằng điểm
+    public static String isSamePoint(int score1) {
         String score = "";
-        switch (score1)
-        {
+        switch (score1) {
             case 0:
                 score = "Love-All";
                 break;
@@ -31,39 +33,46 @@ public class TennisGame {
             default:
                 score = "Deuce";
                 break;
-        }return score;
-    }//khi 2 người chơi bằng điểm
-    public static String over3Point(int score1,int score2){
-        String score ="";
-        int minusResult = score1-score2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
+        }
         return score;
-    }//khi 2 người có điểm số lớn hơn 3
-    public static String under3Point(int score1, int score2){
-        int tempScore=0;
-        String score ="";
-        for (int i=1; i<3; i++)
-        {
-            if (i==1) tempScore = score1;
-            else { score+="-"; tempScore = score2;}
-            switch(tempScore)
-            {
+    }
+
+    //khi 2 người có điểm số lớn hơn 3
+    public static String over3Point(int score1, int score2) {
+        String score = "";
+        int minusResult = score1 - score2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    //khi 2 người chơi có số điểm dưới 3
+    public static String under3Point(int score1, int score2) {
+        int tempScore = 0;
+        String score = "";
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = score1;
+            else {
+                score += "-";
+                tempScore = score2;
+            }
+            switch (tempScore) {
                 case 0:
-                    score+="Love";
+                    score += "Love";
                     break;
                 case 1:
-                    score+="Fifteen";
+                    score += "Fifteen";
                     break;
                 case 2:
-                    score+="Thirty";
+                    score += "Thirty";
                     break;
                 case 3:
-                    score+="Forty";
+                    score += "Forty";
                     break;
             }
-        }return score;
-    }//khi 2 người chơi có số điểm dưới 3
+        }
+        return score;
+    }
 }
