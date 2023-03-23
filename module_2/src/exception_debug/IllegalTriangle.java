feb3ea9c28bde2d9ce84783ef98a40e88e419590
Truongan96dn <hotruongandn@gmail.com>
2023-03-23 16:02:18 +0700

    exception exe

---
+++ module_2/src/exception_debug/IllegalTriangle.java
@@ -0,0 +1,31 @@
+package exception_debug;
+
+import java.util.Scanner;
+
+public class IllegalTriangle {
+
+    public void checkTriangle() throws IllegalTriangleException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập 3 cạnh của tam giác  ");
+        System.out.println("Nhập cạnh a : ");
+        float a = Float.parseFloat(scanner.nextLine());
+        System.out.println("Nhập cạnh b : ");
+        float b = Float.parseFloat(scanner.nextLine());
+        System.out.println("Nhập cạnh c : ");
+        float c = Float.parseFloat(scanner.nextLine());
+        if (a < 0 || b < 0 || c < 0 || a + b < c || a + c < b || b + c < a) {
+            System.out.println(" ba cạnh nhập vào không tạo ra tam giác ");
+        } else {
+            System.out.println("Ba cạnh nhập vào tạo ra tam giác ");
+        }
+    }
+
+    public static void main(String[] args) throws IllegalTriangleException {
+        IllegalTriangle illegalTriangle = new IllegalTriangle();
+        try {
+            illegalTriangle.checkTriangle();
+        } catch (IllegalTriangleException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ module_2/src/exception_debug/IllegalTriangleException.java
@@ -0,0 +1,8 @@
+package exception_debug;
+
+public class IllegalTriangleException extends Exception{
+    public IllegalTriangleException(String str){
+        super(str);
+    }
+
+}
